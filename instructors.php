c062295e27a640a8fa250b6dcba75087b6269fdf
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-23 13:39:01 -0500

    Create instructors.php

---
+++ instructors.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-instructors.php");
+
+$pageTitle="Instructors";
+include "view-header.php";
+$instructors = selectInstructors();
+include "view-instructors.php";
+include "view-footer.php";
+?>
