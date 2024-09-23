b8afaf118c4115ee6591f599e1fea7b1712f8bcf
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-23 13:20:30 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,16 +20,13 @@
                 <a class="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="post.php">Post</a>
+                <a class="nav-link" href="instructors.php">Instructors</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="post-result.php">Post result</a>
+                <a class="nav-link" href="courses.php">Courses</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="get.php">Get</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="get-result.php">Get result</a>
+                <a class="nav-link" href="instructor-with-courses.php">Instructors with courses</a>
               </li>
             </ul>
           </div>
