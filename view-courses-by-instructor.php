733f4aa1950be4da0b2677a0e042d3cad2350372
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-25 13:44:34 -0500

    Create view-courses-by-instructor.php

---
+++ view-courses-by-instructor.php
@@ -0,0 +1,32 @@
+<h1>Courses by Instructor</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Course Number</th>
+      <th>Description</th> 
+      <th>Semester</th> 
+      <th>Room</th> 
+      <th>Day Time</th> 
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while($courses=$courses->fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $course["course_id"];?></td> 
+    <td><?php echo $course["course_number"];?></td>
+    <td><?php echo $course["course_description"];?></td>
+    <td><?php echo $course["semester"];?></td>
+    <td><?php echo $course["room"];?></td>
+    <td><?php echo $course["day_time"];?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
