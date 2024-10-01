999074831fe231ae9ad6761b36180b86016bc86b
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 10:25:31 -0500

    Update sections-by-course.php

--- sections-by-course.php
+++ sections-by-course.php
@@ -4,7 +4,7 @@ require_once("model-sections-by-course.php");
 
 $pageTitle="Sections by Course";
 include "view-header.php";
-$sections = selectSectionsByCourse($_POST['Cid']);
+$sections = selectSectionsByCourse($_POST['cid']);
 include "view-sections-by-course.php";
 include "view-footer.php";
 ?>
