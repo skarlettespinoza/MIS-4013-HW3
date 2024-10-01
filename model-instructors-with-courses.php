fedd709dc7badca8a78a4723ed0cf503d72469d8
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 10:36:51 -0500

    Update model-instructors-with-courses.php

--- model-instructors-with-courses.php
+++ model-instructors-with-courses.php
@@ -12,4 +12,19 @@ function selectInstructors() {
         throw $e;
     }
 }
+
+function selectCoursesByInstructor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT C.course_id, course_number, course_description, semester, room, day_time FROM `course` C JOIN section S ON S.course_id=C.course_id WHERE S.instructor_id=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
