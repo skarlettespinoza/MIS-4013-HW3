abdbea35cedb30f0037a6136a0f46734ff569863
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 10:49:16 -0500

    Create view-instructors-with-courses.php

---
+++ view-instructors-with-courses.php
@@ -0,0 +1,24 @@
+<h1>Instructors with courses</h1>
+<div class="card-group">
+<?php
+while($instructor=$instructors->fetch_assoc()){
+?>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $instructor["instructor_name"];?></h5>
+      <p class="card-text">
+<?php
+  $courses = selectCoursesByInstructor($instructor["instructor_id"]);
+  while($course = $courses->fetch_assoc()){
+?>
+<?php    
+  }
+?> 
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Office: <?php echo $instructor["office_number"];?></small></p>
+    </div>
+  </div> 
+<?php
+}
+?>
+</div>
