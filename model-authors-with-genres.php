2f17ccd3aaf6ce7ee85515a1c47ff9725c7b7515
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 16:19:55 -0500

    Update model-authors-with-genres.php

--- model-authors-with-genres.php
+++ model-authors-with-genres.php
@@ -16,7 +16,7 @@ function selectAuthors() {
 function selectGenresByAuthor($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT G.genre_id, genre, title, book_series, publication_date FROM `genre` G JOIN book B ON B.genre_id=G.genre_id WHERE B.author_id=?");
+        $stmt = $conn->prepare("SELECT G.genre_id, genre, title, publication_date FROM `genre` G JOIN book B ON B.genre_id=G.genre_id WHERE B.author_id=?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
