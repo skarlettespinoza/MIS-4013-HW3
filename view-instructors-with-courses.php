43fdd6acd7f50e890f5181570efe1cb895dd12f0
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 12:10:35 -0500

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -1,26 +1,23 @@
 <h1>Instructors with courses</h1>
 <div class="card-group">
 <?php
-while($instructor = $instructors->fetch_assoc()){
+while($instructor=$instructors->fetch_assoc()){
 ?>
   <div class="card">
     <div class="card-body">
       <h5 class="card-title"><?php echo $instructor["instructor_name"];?></h5>
       <p class="card-text">
-      <ul class="list-group">
 <?php
   $courses = selectCoursesByInstructor($instructor["instructor_id"]);
-  while($course = $courses->fetch_assoc()){
+  while ($course =  $courses->fetch_assoc()){
 ?>
-    <li class="list-group-item"><?php echo $course["course_number"];?> - <?php echo $course["semester"];?> - <?php echo $course["room"];?> - <?php echo $course["day_time"];?></li>
-<?php    
+<?php
   }
-?> 
-      </ul>  
+?>
       </p>
       <p class="card-text"><small class="text-body-secondary">Office: <?php echo $instructor["office_number"];?></small></p>
     </div>
-  </div> 
+  </div>
 <?php
 }
 ?>
