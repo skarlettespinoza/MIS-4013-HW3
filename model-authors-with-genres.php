bf3b20c9ba0bd4e403fd63072e432135cb88ae4d
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 14:00:19 -0500

    Update and rename model-instructors-with-courses.php to model-authors-with-genres.php

--- model-authors-with-genres.php
+++ model-authors-with-genres.php
@@ -1,8 +1,8 @@
 <?php
-function selectInstructors() {
+function selectAuthors() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT instructor_id, instructor_name, office_number FROM `instructor`");
+        $stmt = $conn->prepare("SELECT author_id, author_name, publisher_name FROM `author`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -13,10 +13,10 @@ function selectInstructors() {
     }
 }
 
-function selectCoursesByInstructor($iid) {
+function selectGenresByAuthor($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT C.course_id, course_number, course_description, semester, room, day_time FROM `course` C JOIN section S ON S.course_id=C.course_id WHERE S.instructor_id=?");
+        $stmt = $conn->prepare("SELECT G.genre_id, genre, title, book_series, publication_date FROM `genre` G JOIN book B ON B.genre_id=G.genre_id WHERE B.author_id=?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
