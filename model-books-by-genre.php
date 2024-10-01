5e22df8adb2c71c192dafba2f5021aaa4399b1c5
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 14:11:27 -0500

    Update and rename model-sections-by-course.php to model-books-by-genre.php

---
+++ model-books-by-genre.php
@@ -0,0 +1,16 @@
+<?php
+function selectBooksByGenre($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT G.genre_id, genre, title, book_series, publication_date FROM `genre` G JOIN book B ON B.genre_id=G.genre_id WHERE B.genre_id=?;");
+        $stmt->bind_param("i", $gid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
