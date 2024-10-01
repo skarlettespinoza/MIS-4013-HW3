7d07020c32be5313bc482c3a46db8aaa36dc17e1
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 13:51:53 -0500

    Update and rename courses-by-instructor.php to genres-by-author.php

---
+++ genres-by-author.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-genres-by-author.php");
+
+$pageTitle="Genres by Author";
+include "view-header.php";
+$courses = selectGenresByAuthor($_GET['id']);
+include "view-genres-by-author.php";
+include "view-footer.php";
+?>
