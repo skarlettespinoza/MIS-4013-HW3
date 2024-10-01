8108d6fbe1b137c6a5e2f0185334f37056ab4b6c
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 13:39:54 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
                 <a class="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="instructors.php">Instructors</a>
+                <a class="nav-link" href="instructors.php">Authors</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="courses.php">Courses</a>
