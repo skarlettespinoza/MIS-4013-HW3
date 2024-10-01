f2675bbf6af040bc7faf6f5699879c9441570a15
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-01 14:08:38 -0500

    Update and rename view-instructors-with-courses.php to view-authors-with-genres.php

---
+++ view-authors-with-genres.php
@@ -0,0 +1,27 @@
+<h1>Authors with genres</h1>
+<div class="card-group">
+<?php
+while($author=$authors->fetch_assoc()){
+?>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $author["author_name"];?></h5>
+      <p class="card-text">
+      <ul class="list-group">
+<?php
+  $courses = selectGenresByAuthor($author["author_id"]);
+  while ($genre =  $genres->fetch_assoc()){
+?>
+    <li class="list-group-item"><?php echo $genre["genre"];?> - <?php echo $genre["title"];?> - <?php echo $genre["book_series"];?> - <?php echo $genre["publication_date"];?></li>
+<?php
+  }
+?>
+      </ul>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Publisher: <?php echo $author["publisher_name"];?></small></p>
+    </div>
+  </div>
+<?php
+}
+?>
+</div>
