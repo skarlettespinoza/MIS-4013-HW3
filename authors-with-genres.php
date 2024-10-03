21d5231e8387df0f97a72f572710252b78a594a6
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-03 09:24:56 -0500

    Update authors-with-genres.php

--- authors-with-genres.php
+++ authors-with-genres.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-authors-with-genres.php");
 
-$pageTitle="Authors with Genres";
+$pageTitle="Author's Books with Genres";
 include "view-header.php";
 $authors = selectAuthors();
 include "view-authors-with-genres.php";
