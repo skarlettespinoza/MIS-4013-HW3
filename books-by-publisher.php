5270ef3f09b482710b74545a3bc7fe93f3939852
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-03 10:23:39 -0500

    Update and rename authors-by-publisher.php to books-by-publisher.php

---
+++ books-by-publisher.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-books-by-publisher.php");
+
+$pageTitle="Books by Publisher";
+include "view-header.php";
+$authors = selectBooksByPublisher($_POST['pid']);
+include "view-books-by-publisher.php";
+include "view-footer.php";
+?>
