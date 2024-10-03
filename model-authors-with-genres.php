eb8765517b40af3fac4363b55b9eb6f6cf7ac9bb
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-03 09:21:36 -0500

    Update model-authors-with-genres.php

--- model-authors-with-genres.php
+++ model-authors-with-genres.php
@@ -1,5 +1,17 @@
 <?php
-
+function selectAuthors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT author_id, author_name, publisher_name FROM `author`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 function selectGenresByAuthor($iid) {
     try {
         $conn = get_db_connection();
