0a0ac4783d8f0add2dbf4e16d89bc197867a584a
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-03 10:04:35 -0500

    Update books-with-publishers.php

--- books-with-publishers.php
+++ books-with-publishers.php
@@ -1,10 +1,10 @@
-<?php
-require_once("util-db.php");
-require_once("model-books-with-publishers.php");
+// <?php
+// require_once("util-db.php");
+// require_once("model-books-with-publishers.php");
 
-$pageTitle="Books with Publishers";
-include "view-header.php";
-$books = selectBooks();
-include "view-books-with-publishers.php";
-include "view-footer.php";
-?>
+// $pageTitle="Books with Publishers";
+// include "view-header.php";
+// $books = selectBooks();
+// include "view-books-with-publishers.php";
+// include "view-footer.php";
+// ?>
