713ab33df052b0695778ef2b9f7a654f2ca01d22
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-07 14:15:00 -0500

    Update view-authors.php

--- view-authors.php
+++ view-authors.php
@@ -5,7 +5,8 @@
       <tr>
       <th>ID</th>
       <th>Name</th>
-      <th>Publisher</th> 
+      <th>Birthdate</th> 
+      <th>Age</th> 
         <th></th>
       </tr>
     </thead>
@@ -16,7 +17,8 @@ while($author=$authors->fetch_assoc()){
   <tr>
     <td><?php echo $author["author_id"];?></td> 
     <td><?php echo $author["author_name"];?></td>
-    <td><?php echo $author["publisher_name"];?></td>
+    <td><?php echo $author["author_birthdate"];?></td>
+    <td><?php echo $author["author_age"];?></td>
     <td><a href="genre-by-author.php?id=<?php echo $author["author_id"];?>">Genre</a></td>
   </tr>
 <?php
