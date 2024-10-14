772bd18f62fa1adfc8edd3840ca39535bb0eedc8
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-14 13:41:22 -0500

    Update genres.php

--- genres.php
+++ genres.php
@@ -4,6 +4,15 @@ require_once("model-genres.php");
 
 $pageTitle="Genres";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertGenres($_POST['gGen'];
+      break;
+  }
+}
+
 $genres = selectGenres();
 include "view-genres.php";
 include "view-footer.php";
