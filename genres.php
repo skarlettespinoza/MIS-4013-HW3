f969b93ecdd91a21254a85bbe834793bca1cb76c
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-14 13:46:57 -0500

    Update genres.php

--- genres.php
+++ genres.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
-      insertGenres($_POST['gGen'];
+      if(insertGenres($_POST['gGen'])){
+        echo '<div class="alert alert-success" role="alert">Genre added</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
       break;
   }
 }
