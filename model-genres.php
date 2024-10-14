85a6d7650f7b09719be0a1a39d3e96924347d341
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-14 12:53:16 -0500

    Update model-genres.php

--- model-genres.php
+++ model-genres.php
@@ -12,4 +12,46 @@ function selectGenres() {
         throw $e;
     }
 }
+
+function insertGenres($gGen) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `genre` (`genre`) VALUES (?);");
+        $stmt->bind_param("s", $gGen);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateGenres($gGen, $gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `genre` set genre = ? where genre_id = ?");
+        $stmt->bind_param("si", $gGen, $gid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteGenres($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from genre where genre_id=?");
+        $stmt->bind_param("i", $gid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
