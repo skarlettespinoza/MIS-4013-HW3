f8a4b39fe9490895b5f9172db89f9e13511a198b
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-16 17:28:36 -0500

    Update model-books.php

--- model-books.php
+++ model-books.php
@@ -2,7 +2,7 @@
 function selectBooks() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT book_id, author_id, genre_id, publisher_id title, book_series, publication_date FROM `book`");
+        $stmt = $conn->prepare("SELECT book_id, author_id, genre_id, publisher_id, title, book_series, publication_date FROM `book`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
