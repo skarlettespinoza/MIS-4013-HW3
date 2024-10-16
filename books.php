aa499939e1ac77a1f6d69750c55797516405e779
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-16 17:17:44 -0500

    Update books.php

--- books.php
+++ books.php
@@ -5,6 +5,6 @@ require_once("model-books.php");
 $pageTitle="Books";
 include "view-header.php";
 $books = selectBooks();
-include "view-authors.php";
+include "view-books.php";
 include "view-footer.php";
 ?>
