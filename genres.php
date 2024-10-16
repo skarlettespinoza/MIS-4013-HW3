3cedb73d2f340ecec4ba262023b48efe5fe9e4db
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-16 16:41:06 -0500

    Update genres.php

--- genres.php
+++ genres.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])){
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
+        case "Delete":
+      if(deleteGenres($_POST['gid'])){
+        echo '<div class="alert alert-success" role="alert">Genre deleted</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
   }
 }
 
