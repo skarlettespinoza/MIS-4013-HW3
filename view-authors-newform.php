3204dd0ca78442d4a78f508c9442ec9b8077dd46
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-18 12:49:57 -0500

    Update view-authors-newform.php

--- view-authors-newform.php
+++ view-authors-newform.php
@@ -1,4 +1,36 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newAuthorModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
     </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newAuthorModal" tabindex="-1" aria-labelledby="newAuthorModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newAuthorModalLabel">New author</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="aName" class="form-label">Author name</label>
+            <input type="text" class="form-control" id="aName" name="aName">
+          </div>
+        <div class="mb-3">
+            <label for="aBirthdate" class="form-label">Author birthdate</label>
+            <input type="text" class="form-control" id="aBirthdate" name="aBirthdate">
+          </div>
+        <div class="mb-3">
+            <label for="aAge" class="form-label">Author age</label>
+            <input type="number" class="form-control" id="aAge" name="aAge">
+        </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
