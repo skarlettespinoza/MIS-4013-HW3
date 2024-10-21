c28191b89081a3ad8d8d6dc760fb8921100f60ea
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-21 16:50:28 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -14,13 +14,13 @@ if (isset($_POST['actionType'])){
         echo'<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
-        case "Delete":
-      if(deleteAuthors($_POST['aid'])){
-        echo '<div class="alert alert-success" role="alert">Author deleted</div>';
+    case "Delete":
+      if(deleteAuthors($_POST['aid'])) {
+        echo'<div class="alert alert-success" role="alert">Author deleted</div>';
       } else {
-        echo '<div class="alert alert-danger" role="alert">Error</div>';
+        echo'<div class="alert alert-danger" role="alert">Error</div>';
       }
-      break;  
+      break;
     
   }
 }
