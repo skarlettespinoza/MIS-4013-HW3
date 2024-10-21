c1172e77c53a772960730adb0b746ebdeb307fa4
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-21 16:02:53 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -4,6 +4,15 @@ require_once("model-authors.php");
 
 $pageTitle="Authors";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertAuthors($_POST['aName'],$_POST['aBirthdate'],$_POST['aAge']);
+      break;
+    
+  }
+}
 $authors = selectAuthors();
 include "view-authors.php";
 include "view-footer.php";
