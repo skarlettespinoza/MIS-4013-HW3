84c0e7dd44db6f581a9fc237c27b26c2e25b3943
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-21 19:19:01 -0500

    Update model-publishers.php

--- model-publishers.php
+++ model-publishers.php
@@ -12,4 +12,47 @@ function selectPublishers() {
         throw $e;
     }
 }
+
+function insertPublishers($pName, $pLoc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(INSERT INTO `publisher` (`publisher_name`,`location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $pName, $pLoc);
+        $stmt->execute();
+        $success = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updatePublishers($pName, $pLoc, $pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `publisher` set `publisher_name`=?,`location`=? where publisher_id=?");
+        $stmt->bind_param("ssi", $pName, $pLoc, $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletePublishers($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from publisher where publisher_id=?");
+        $stmt->bind_param("i", $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
