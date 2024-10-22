da1048f14c712db63bf0ef6a85d5c5fd7cd01553
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-22 12:40:27 -0500

    Update view-publishers-newform.php

--- view-publishers-newform.php
+++ view-publishers-newform.php
@@ -24,6 +24,7 @@
             <label for="pLoc" class="form-label">Location</label>
             <input type="text" class="form-control" id="pLoc" name="pLoc">
           </div>
+            <input type="hidden" name="actionType" value="Add">
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
