d375f9b34997b080cbb98934facf29a0df7b823f
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-21 19:46:29 -0500

    Update publishers.php

--- publishers.php
+++ publishers.php
@@ -4,16 +4,6 @@ require_once("model-publishers.php");
 
 $pageTitle="Publishers";
 include "view-header.php";
-
-if (isset($_POST['actionType'])){
-  switch ($_POST['actionType']) {
-    case "Add":
-      if(insertPublishers($_POST['pName'],$_POST['pLoc'])) {
-        echo'<div class="alert alert-success" role="alert">Publisher added</div>';
-      } else {
-        echo'<div class="alert alert-danger" role="alert">Error</div>';
-      }
-      break;
 $publishers = selectPublishers();
 include "view-publishers.php";
 include "view-footer.php";
