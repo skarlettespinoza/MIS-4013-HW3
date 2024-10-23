a7b390a8495f6e3ce1734d718f58a970af0e4fdc
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-23 12:24:01 -0500

    Update model-authors-with-genres.php

--- model-authors-with-genres.php
+++ model-authors-with-genres.php
@@ -26,4 +26,46 @@ function selectGenresByAuthor($iid) {
         throw $e;
     }
 }
+
+function insertBooks($iid, $gid, $pid, $title, $book_series, $publication_date){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `book` (`author_id`, `genre_id`, `publisher_id`, `title`, `book_series`, `publication_date`) VALUES (?,?,?,?,?,?)");
+        $stmt->bind_param("iiisss", $iid, $gid, $pid, $title, $book_series, $publication_date);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}    
+
+function updateBooks($iid, $gid, $pid, $title, $book_series, $publication_date, $bid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `book` set `author_id`=?,`genre_id`=?,`publisher_id`=?, `title`=?, `book_series`=?, `publication_date`=? where book_id=?");
+        $stmt->bind_param("iiisssi", $iid, $gid, $pid, $title, $book_series, $publication_date, $bid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteBooks($bid){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from book where book_id=?");
+        $stmt->bind_param("i", $bid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
