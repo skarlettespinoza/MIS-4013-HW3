b35ae498b37317b1691e0fab58e77a7acaefe8ca
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-23 12:50:27 -0500

    Create view-authors-with-genres-newform.php

---
+++ view-authors-with-genres-newform.php
@@ -0,0 +1,48 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newBookModal">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+  </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newBookModal" tabindex="-1" aria-labelledby="newBookModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newBookModalLabel">New book</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <div class="mb-3">
+            <label for="iid" class="form-label">Author</label>
+            <input type="number" class="form-control" id="iid" name="iid">
+          </div>
+          <div class="mb-3">
+            <label for="gid" class="form-label">Genre</label>
+            <input type="number" class="form-control" id="gid" name="gid">
+          </div>
+          <div class="mb-3">
+            <label for="pid" class="form-label">Publisher</label>
+            <input type="number" class="form-control" id="pid" name="pid">
+          </div>
+          <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" name="title">
+          </div>    
+          <div class="mb-3">
+            <label for="book_series" class="form-label">Book Series</label>
+            <input type="text" class="form-control" id="book_series" name="book_series">
+          </div>    
+          <div class="mb-3">
+            <label for="publication_date" class="form-label">Publication Date</label>
+            <input type="text" class="form-control" id="publication_date" name="publication_date">
+          </div>          
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
