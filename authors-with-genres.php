ed95dd0048f454e56ff0296f1868547fc4dde43b
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-23 13:05:32 -0500

    Update authors-with-genres.php

--- authors-with-genres.php
+++ authors-with-genres.php
@@ -4,6 +4,16 @@ require_once("model-authors-with-genres.php");
 
 $pageTitle="Authors with Genres";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      if(insertBooks($_POST['iid'],$_POST['gid'],$_POST['pid'],$_POST['title'],$_POST['book_series'],$_POST['publication_date'])) {
+        echo'<div class="alert alert-success" role="alert">Book added</div>';
+      } else {
+        echo'<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
 $authors = selectAuthors();
 include "view-authors-with-genres.php";
 include "view-footer.php";
