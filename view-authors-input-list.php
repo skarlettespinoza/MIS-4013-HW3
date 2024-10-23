4304835d296c16049f1abc5a3b71d103ff4e989a
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-23 17:02:41 -0500

    Update view-authors-input-list.php

--- view-authors-input-list.php
+++ view-authors-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="iid" name="iid">
 <?php
 while ($authorsItem = $authorsList->fetch_assoc()) {
+  $selText = "";
+  if($selectedAuthors==$authorsItem['author_id']){
+    $selText="selected";
+  }
 ?>
-  <option value="<?php echo $authorsItem['author_id']; ?>"><?php echo $authorsItem['author_name']; ?></option>  
+  <option value="<?php echo $authorsItem['author_id']; ?>"<?=$selText?>><?php echo $authorsItem['author_name']; ?></option>  
 <?php
 }
 ?>
