2368759aa6b8a535c983439ef2a10a04ea8f22dc
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-23 17:17:26 -0500

    Update authors-with-genres.php

--- authors-with-genres.php
+++ authors-with-genres.php
@@ -14,9 +14,16 @@ if (isset($_POST['actionType'])){
         echo'<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
+     case "Delete":
+      if(deleteBooks($_POST['iid'],$_POST['gid'],$_POST['pid'],$_POST['title'],$_POST['book_series'],$_POST['publication_date'])) {
+        echo'<div class="alert alert-success" role="alert">Book added</div>';
+      } else {
+        echo'<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;   
     case "Edit":
-      if(updateBooks($_POST['iid'],$_POST['gid'],$_POST['pid'],$_POST['title'],$_POST['book_series'],$_POST['publication_date'],$_POST['bid'])) {
-        echo'<div class="alert alert-success" role="alert">Book edited</div>';
+      if(updateBooks($_POST['bid'])) {
+        echo'<div class="alert alert-success" role="alert">Book deleted</div>';
       } else {
         echo'<div class="alert alert-danger" role="alert">Error</div>';
       }
