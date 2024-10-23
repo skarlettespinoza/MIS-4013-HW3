6500472b1bfb27be8c9682a4325eb9cd41ec88ed
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-23 14:29:00 -0500

    Update authors-with-genres.php

--- authors-with-genres.php
+++ authors-with-genres.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
-      if(insertBooks($_POST['iid'],$_POST['gid'],$_POST['pid'],$_POST['title'],$_POST['book_series'],$_POST['publication_date'])) {
+      if(insertBooks($_POST['iid'],$_POST['gid'],$_POST['pid'],$_POST['title'],$_POST['book_series'],$_POST['publication_date'],$_POST['bid'])) {
         echo'<div class="alert alert-success" role="alert">Book added</div>';
       } else {
         echo'<div class="alert alert-danger" role="alert">Error</div>';
