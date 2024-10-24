7d8c2829d1e8b4f847a147e6e444aac51f80f1ca
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-10-24 13:05:35 -0500

    Update view-genres-input-list.php

--- view-genres-input-list.php
+++ view-genres-input-list.php
@@ -1,8 +1,13 @@
 <select class="form-select" id="gid" name="gid">
 <?php
-while ($genresItem = $genresList->fetch_assoc()){
+while ($genresItem = $genresList->fetch_assoc()) {
+  $selText = "";
+  if($selectedGenres==$GenresItem['genre_id']){
+    $selText="selected";
+  }
 ?>
-    <option value="<?php echo $genresItem['genre_id']; ?>"><?php echo $genresItem['genre']; ?></option>
-<?php  
-} 
+  <option value="<?php echo $genresItem['genre_id']; ?>"<?=$selText?>><?php echo $genresItem['genre']; ?></option>  
+<?php
+}
 ?>
+</select>
