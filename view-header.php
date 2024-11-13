29324256047b24bc63aa5f3353670b3cbd3435d1
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-13 13:55:13 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,38 +8,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/quartz/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <div class="container">
-      <!-- Navbar without rounded corners -->
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="authors.php">Authors</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="genres.php">Genres</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="publishers.php">Publishers</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="authors-with-genres.php">Authors with genres</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="books.php">Books</a>
-              </li>
-            </ul>
-          </div>
+    <!-- Navbar spanning the full width of the page -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary container-fluid">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="authors.php">Authors</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="genres.php">Genres</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="publishers.php">Publishers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="authors-with-genres.php">Authors with genres</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="books.php">Books</a>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
+
+    <!-- Main content container -->
+    <div class="container">
+      <!-- Your main page content goes here -->
     </div>
 
     <!-- Bootstrap JS (optional for interactive components) -->
