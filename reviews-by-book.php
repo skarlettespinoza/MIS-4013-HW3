f6f642901ed655fd58da2a8bde7044696bb239a2
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-14 11:54:32 -0600

    Update reviews-by-book.php

--- reviews-by-book.php
+++ reviews-by-book.php
@@ -2,9 +2,12 @@
 require_once("util-db.php");
 require_once("model-reviews-by-book.php");
 
-$pageTitle="Reviews by Book";
+$pageTitle = "Reviews by Book";
 include "view-header.php";
-$reviews = selectReviewsByBook($_GET['rid']);
+
+// Corrected the parameter to use $_GET['id'] instead of $_GET['rid']
+$reviews = selectReviewsByBook($_GET['id']);
+
 include "view-reviews-by-book.php";
 include "view-footer.php";
 ?>
