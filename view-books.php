f51dae4ce7f380ba4af5974e11420211b0f4b0a8
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-14 11:42:25 -0600

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -19,40 +19,7 @@ while($book=$books->fetch_assoc()){
     <td><?php echo $book["title"];?></td>
     <td><?php echo $book["book_series"];?></td>
     <td><?php echo $book["publication_date"];?></td>
-<h1>Books</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-      <th>ID</th>
-      <th>Title</th> 
-      <th>Book Series</th>
-      <th>Publication Date</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-<?php
-while($book=$books->fetch_assoc()){
-?>
-  <tr>
-    <td><?php echo $book["book_id"];?></td> 
-    <td><?php echo $book["title"];?></td>
-    <td><?php echo $book["book_series"];?></td>
-    <td><?php echo $book["publication_date"];?></td>
-    <td>
-      <form method="post" action="reviews-by-book.php">
-        <input type="hidden" name="book_id" value="<?php echo $book["book_id"];?>">
-        <button type="submit" class="btn btn-primary">Reviews</button>
-      </form>
-    </td>
-  </tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
-</div>
+    <td><a href="publisher-by-book.php?id=<?php echo $book["book_id"];?>">Publisher</a></td>
   </tr>
 <?php
 }
