368dc97b137f2c50c21499e7454aec08c6334e56
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-14 11:53:56 -0600

    Update reviews-by-book.php

--- reviews-by-book.php
+++ reviews-by-book.php
@@ -4,7 +4,7 @@ require_once("model-reviews-by-book.php");
 
 $pageTitle="Reviews by Book";
 include "view-header.php";
-$reviews = selectReviewsByBook($_POST['rid']);
+$reviews = selectReviewsByBook($_GET['rid']);
 include "view-reviews-by-book.php";
 include "view-footer.php";
 ?>
