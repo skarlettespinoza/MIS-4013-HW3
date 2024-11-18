625f5e834adb65db1545c5e95bbca4cbbc28ce33
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-18 14:30:54 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -4,37 +4,51 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Link to Bootswatch Minty theme -->
+    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/minty/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <div class="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="authors.php">Authors</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="genres.php">Genres</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="publishers.php">Publishers</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="authors-with-genres.php">Authors with genres</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="books.php">Books</a>
-              </li>
-            </ul>
-          </div>
+    <!-- Navbar with the primary color from the Minty theme -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary container-fluid">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="authors.php">Authors</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="genres.php">Genres</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="publishers.php">Publishers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="authors-with-genres.php">Authors with genres</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="books.php">Books</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="books-with-reviews.php">Reviews</a>
+            </li>            
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
+
+    <!-- Main content container -->
+    <div class="container">
+      <!-- Your main page content goes here -->
+    </div>
+
+    <!-- Bootstrap JS (optional for interactive components) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+e4I7kPsR93p1kD80Psrw5Yp1g6g7" crossorigin="anonymous"></script>
+  </body>
+</html>
