4fca013c7c8745eec5fe3e6b45efabe3d6b20d91
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-18 15:02:35 -0600

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -7,6 +7,7 @@
       <th>Title</th> 
       <th>Book Series</th>
       <th>Publication Date</th>
+      <th>Review</th>
         <th></th>
       </tr>
     </thead>
@@ -19,6 +20,7 @@ while($book=$books->fetch_assoc()){
     <td><?php echo $book["title"];?></td>
     <td><?php echo $book["book_series"];?></td>
     <td><?php echo $book["publication_date"];?></td>
+    <td><?php echo $book["review"];?></td>    
     <td><a href="publisher-by-book.php?id=<?php echo $book["book_id"];?>">Publisher</a></td>
   </tr>
 <?php
