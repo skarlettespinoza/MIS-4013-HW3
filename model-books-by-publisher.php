d6baeaac39365df0a36cbf92dd1d5c77d3fb910f
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-18 14:55:16 -0600

    Update model-books-by-publisher.php

--- model-books-by-publisher.php
+++ model-books-by-publisher.php
@@ -2,7 +2,7 @@
 function selectBooksByPublisher($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT P.publisher_id, publisher_name, location, title, book_series, publication_date FROM `publisher` P JOIN book B ON B.publisher_id=P.publisher_id WHERE B.publisher_id=?;");
+        $stmt = $conn->prepare("SELECT P.publisher_id, publisher_name, location, title, book_series, publication_date, review FROM `publisher` P JOIN book B ON B.publisher_id=P.publisher_id WHERE B.publisher_id=?;");
         $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
