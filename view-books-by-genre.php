e9ecd93f45dd947ef022053e67ec8e7dadb593ad
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-11-27 10:33:03 -0600

    Update view-books-by-genre.php

--- view-books-by-genre.php
+++ view-books-by-genre.php
@@ -1,21 +1,31 @@
-<h1>Books by genre</h1>
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $genre_name = isset($_POST['genre_name']) ? htmlspecialchars($_POST['genre_name']) : "Unknown Genre";
+    $gid = isset($_POST['gid']) ? intval($_POST['gid']) : 0;
+
+    // Query the books for the given genre ID
+    // (Assuming you already have the logic to fetch $books based on $gid)
+}
+?>
+
+<h1>Books in Genre: <?php echo $genre_name; ?></h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-      <th>Genre</th>
-      <th>Title</th>
-      <th>Book Series</th>
+        <th>Genre</th>
+        <th>Title</th>
+        <th>Book Series</th>
       </tr>
     </thead>
     <tbody>
 <?php
-while($book=$books->fetch_assoc()){
+while ($book = $books->fetch_assoc()) {
 ?>
   <tr>
-    <td><?php echo $book["genre"];?></td>
-    <td><?php echo $book["title"];?></td>
-    <td><?php echo $book["book_series"];?></td> 
+    <td><?php echo $book["genre"]; ?></td>
+    <td><?php echo $book["title"]; ?></td>
+    <td><?php echo $book["book_series"]; ?></td>
   </tr>
 <?php
 }
