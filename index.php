2c281065bed60b08bb063457564c3537c693ac98
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-12-01 13:47:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "view-header.php";
 ?>
 <div style="position: relative; height: 100vh; display: flex; justify-content: center; align-items: center; text-align: center;">
   <!-- Image Layer -->
-  <img src="https://www.onlygfx.com/wp-content/uploads/2018/01/colorful-flower-frame-rectangle-2-1024x713.png" 
+  <img src="https://i.pinimg.com/originals/4d/e5/81/4de581575875feb1ef36ac556596d31a.jpg" 
        alt="Library Logo" 
        style="position: absolute; width: 800px; height: auto; z-index: 1; max-width: 100%;">
 
