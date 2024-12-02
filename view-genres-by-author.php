17e9c6286b20053d6b9ab61d215687dfd201cb36
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-12-02 12:55:35 -0600

    Update view-genres-by-author.php

--- view-genres-by-author.php
+++ view-genres-by-author.php
@@ -1,25 +1,24 @@
-<h1>Genres</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-      <th>Genre</th> 
-      <th>Title</th>
-      <th>Book Series</th>
-      </tr>
-    </thead>
-    <tbody>
-<?php
-while($genre=$genres->fetch_assoc()){
-?>
-  <tr>
-    <td><?php echo $genre["genre"];?></td>
-    <td><?php echo $genre["title"];?></td>
-    <td><?php echo $genre["book_series"];?></td> 
-  </tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
+<div class="container my-4">
+  <h1 class="text-center">Genres</h1>
+  
+  <div class="table-responsive mt-4">
+    <table class="table table-bordered table-hover text-center">
+      <thead class="thead-light">
+        <tr>
+          <th>Genre</th>
+          <th>Title</th>
+          <th>Book Series</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php while ($genre = $genres->fetch_assoc()) { ?>
+          <tr>
+            <td><?php echo $genre["genre"]; ?></td>
+            <td><?php echo $genre["title"]; ?></td>
+            <td><?php echo $genre["book_series"]; ?></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
 </div>
