d14185422804bfd9085d789fc080c7f04b6a96be
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-12-02 12:59:45 -0600

    Update view-genres.php

--- view-genres.php
+++ view-genres.php
@@ -1,56 +1,51 @@
-<div class="row">
-  <div class="col">
-<h1>Genres</h1>
+<div class="container my-4">
+  <div class="row justify-content-between align-items-center">
+    <div class="col">
+      <h1 class="text-center">Genres</h1>
+    </div>
+    <div class="col-auto">
+      <?php include "view-genres-newform.php"; ?>
+    </div>
   </div>
-  <div class="col-auto">
-<?php
-include "view-genres-newform.php";
-?>
+
+  <div class="table-responsive mt-4">
+    <table class="table table-bordered table-hover text-center">
+      <thead class="thead-light">
+        <tr>
+          <th>Genre</th>
+          <th>Edit</th>
+          <th>Delete</th>
+          <th>Books</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php while ($genre = $genres->fetch_assoc()) { ?>
+          <tr>
+            <td><?php echo $genre["genre"]; ?></td>
+            <td>
+              <?php include "view-genres-editform.php"; ?>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="gid" value="<?php echo $genre["genre_id"]; ?>">
+                <input type="hidden" name="actionType" value="Delete">
+                <button type="submit" class="btn btn-secondary btn-sm" onclick="return confirm('Are you sure?');">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                  </svg>
+                </button>
+              </form>
+            </td>
+            <td>
+              <form method="post" action="books-by-genre.php">
+                <input type="hidden" name="gid" value="<?php echo $genre["genre_id"]; ?>">
+                <input type="hidden" name="genre_name" value="<?php echo htmlspecialchars($genre["genre"]); ?>">
+                <button type="submit" class="btn btn-info btn-sm">Books</button>
+              </form>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
   </div>
-</div>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-      <th>Genre</th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-<?php
-while($genre=$genres->fetch_assoc()){
-?>
-  <tr>
-    <td><?php echo $genre["genre"];?></td>
-    <td>
-<?php
-include "view-genres-editform.php";
-?>      
-    </td>
-    <td>
-      <form method="post" action="">
-        <input type="hidden" name="gid" value="<?php echo $genre["genre_id"];?>">
-        <input type ="hidden" name="actionType" value="Delete">
-        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-          </svg>
-        </button>
-      </form>
-    </td>
-    <td>
-      <form method="post" action="books-by-genre.php">
-        <input type="hidden" name="gid" value="<?php echo $genre["genre_id"];?>">
-        <input type="hidden" name="genre_name" value="<?php echo htmlspecialchars($genre["genre"]); ?>">
-        <button type="submit" class="btn btn-primary">Books</button>
-      </form>
-    </td>
-  </tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
 </div>
