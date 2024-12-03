72423f7732714cd3409eae4ffefa764f5797cd70
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-12-03 16:57:39 -0600

    Update view-publishers-newform.php

--- view-publishers-newform.php
+++ view-publishers-newform.php
@@ -11,7 +11,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newPublisherModalLabel">New publisher</h1>
+        <h1 class="modal-title fs-5" id="newPublisherModalLabel">New Publisher</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
