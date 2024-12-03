e39db9364475df0d2cb03524f1e8aa2c2cb88cd2
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-12-03 16:33:30 -0600

    Update view-authors-with-genres-editform.php

--- view-authors-with-genres-editform.php
+++ view-authors-with-genres-editform.php
@@ -16,53 +16,53 @@
       </div>
       <div class="modal-body">
         <form method="post" action="">
-          <div class="mb-3">
+          <div class="mb-3 text-start">
             <label for="iid<?php echo $genre['book_id']; ?>" class="form-label">Author</label>
-<?php
-$authorsList = selectAuthorsForInput();
-$selectedAuthors=$genre['author_id'];
-include "view-authors-input-list.php"
-?>
+            <?php
+            $authorsList = selectAuthorsForInput();
+            $selectedAuthors=$genre['author_id'];
+            include "view-authors-input-list.php"
+            ?>
           </div>
-          <div class="mb-3">
+          <div class="mb-3 text-start">
             <label for="gid<?php echo $genre['book_id']; ?>" class="form-label">Genre</label>
-<?php
-$genresList = selectGenresForInput();
-$selectedGenres=$genre['genre_id'];
-include "view-genres-input-list.php"
-?>
+            <?php
+            $genresList = selectGenresForInput();
+            $selectedGenres=$genre['genre_id'];
+            include "view-genres-input-list.php"
+            ?>
           </div>
-          <div class="mb-3">
+          <div class="mb-3 text-start">
             <label for="pid<?php echo $genre['book_id']; ?>" class="form-label">Publisher</label>
-<?php
-$publishersList = selectPublishersForInput();
-$selectedPublishers=$genre['publisher_id'];
-include "view-publishers-input-list.php"
-?>
+            <?php
+            $publishersList = selectPublishersForInput();
+            $selectedPublishers=$genre['publisher_id'];
+            include "view-publishers-input-list.php"
+            ?>
           </div>
-          <div class="mb-3">
+          <div class="mb-3 text-start">
             <label for="title<?php echo $genre['book_id']; ?>" class="form-label">Title</label>
             <input type="text" class="form-control" id="title<?php echo $genre['book_id']; ?>" name="title" value="<?php echo $genre['title']; ?>">
           </div>    
-          <div class="mb-3">
+          <div class="mb-3 text-start">
             <label for="book_series<?php echo $genre['book_id']; ?>" class="form-label">Book Series</label>
             <input type="text" class="form-control" id="book_series<?php echo $genre['book_id']; ?>" name="book_series" value="<?php echo $genre['book_series']; ?>">
           </div>    
-          <div class="mb-3">
+          <div class="mb-3 text-start">
             <label for="publication_date<?php echo $genre['book_id']; ?>" class="form-label">Publication Date</label>
             <input type="text" class="form-control" id="publication_date<?php echo $genre['book_id']; ?>" name="publication_date" value="<?php echo $genre['publication_date']; ?>">
           </div>   
-          <div class="mb-3">
+          <div class="mb-3 text-start">
             <label for="review<?php echo $genre['book_id']; ?>" class="form-label">Review</label>
             <input type="text" class="form-control" id="review<?php echo $genre['book_id']; ?>" name="review" value="<?php echo $genre['review']; ?>">
           </div>           
             <input type="hidden" name="bid" value="<?php echo $genre['book_id']; ?>">          
             <input type="hidden" name="actionType" value="Edit">
-          <button type="submit" class="btn btn-primary">Save</button>
+          <div class="text-start">
+            <button type="submit" class="btn btn-primary">Save</button>
+          </div>
         </form>
       </div>
     </div>
   </div>
 </div>
-
-
