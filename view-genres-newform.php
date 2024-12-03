6f0f61715b8ba2f57829f9db069f7e0a97e12bcd
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-12-03 16:57:58 -0600

    Update view-genres-newform.php

--- view-genres-newform.php
+++ view-genres-newform.php
@@ -11,7 +11,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newGenreModalLabel">New genre</h1>
+        <h1 class="modal-title fs-5" id="newGenreModalLabel">New Genre</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
